918f0e07498279453596d705f82bb31d23f7097e
Marcos Allan <marcosallanm1to@gmail.com>
2025-11-29 13:11:28 -0300

    build: base project

---
+++ src/main/java/dev/marcos/api_job_search/ApiJobSearchApplication.java
@@ -0,0 +1,13 @@
+package dev.marcos.api_job_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJobSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJobSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marcos/api_job_search/ApiJobSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcos.api_job_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJobSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
